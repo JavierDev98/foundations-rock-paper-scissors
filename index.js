a82013dbad0997b718869ce2198723c94f40da2a
JavierDev98 <javiersniper1@gmail.com>
2023-04-29 16:09:27 -0600

    Game's logic has been implemented

---
+++ index.js
@@ -0,0 +1,35 @@
+// 1 = Rock 2 = Paper 3 = Scissors
+
+function getComputerChoice () {
+  let computerSelection = Math.floor(Math.random() * 3 + 1)
+  if (computerSelection === 1) {
+    computerSelection = 'Rock'
+  } else if (computerSelection === 2) {
+    computerSelection = 'Paper'
+  } else if (computerSelection === 3) {
+    computerSelection = 'Scissors'
+  }
+  console.log(computerSelection)
+  return computerSelection
+}
+
+function playRound (playerSelection) {
+  let computer = getComputerChoice();
+  if (playerSelection === computer) {
+    console.log('No one wins')
+  } else if (playerSelection === 'Rock' && computer === 'Paper') {
+    console.log('Paper beats Rock. Computer wins!')
+  } else if (playerSelection === 'Paper' && computer === 'Rock') {
+    console.log('Paper beats Rock. Player wins!')
+  } else if (playerSelection === 'Paper' && computer === 'Scissors') {
+    console.log('Scissors beats Paper. Computer wins!')
+  } else if (playerSelection === 'Scissors' && computer === 'Paper') {
+    console.log('Paper beats Scissors. Player wins!')
+  } else if (playerSelection === 'Scissors' && computer === 'Rock') {
+    console.log('Rock beats Scissors. Computer wins!')
+  } else if (playerSelection === 'Rock' && computer === 'Scissors') {
+    console.log('Rock beats Scissors. Player wins!')
+  }
+}
+
+playRound ("Paper");
