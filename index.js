3581ad33626f647648883be10462fba06199b77a
JavierDev98 <javiersniper1@gmail.com>
2023-05-06 00:43:59 -0600

    added new function that sets images according to choices made

--- index.js
+++ index.js
@@ -4,38 +4,62 @@ function getComputerChoice () {
   let computerSelection = Math.floor(Math.random() * 3 + 1)
   // Here we generate a random number from 1 to 3, then:
   if (computerSelection === 1) {
-    computerSelection = 'Rock'
+    computerSelection = 'Rock';
   } else if (computerSelection === 2) {
-    computerSelection = 'Paper'
+    computerSelection = 'Paper';
   } else if (computerSelection === 3) {
-    computerSelection = 'Scissors'
+    computerSelection = 'Scissors';
   }
-  return computerSelection
+  return computerSelection;
 }
 
 function playRound (playerSelection) { // This function does two things: 1. The logic 2. Display winner/loser/tie through console.log 
   let computer = getComputerChoice();
+  getImages(playerSelection,computer);
   if (playerSelection === computer) {
-    console.log('No one wins')
+    console.log('No one wins');
   } else if (playerSelection === 'Rock' && computer === 'Paper') {
-    console.log('Paper beats Rock. Computer wins!')
+    console.log('Paper beats Rock. Computer wins!');
   } else if (playerSelection === 'Paper' && computer === 'Rock') {
-    console.log('Paper beats Rock. Player wins!')
+    console.log('Paper beats Rock. Player wins!');
   } else if (playerSelection === 'Paper' && computer === 'Scissors') {
-    console.log('Scissors beats Paper. Computer wins!')
+    console.log('Scissors beats Paper. Computer wins!');
   } else if (playerSelection === 'Scissors' && computer === 'Paper') {
-    console.log('Paper beats Scissors. Player wins!')
+    console.log('Paper beats Scissors. Player wins!');
   } else if (playerSelection === 'Scissors' && computer === 'Rock') {
-    console.log('Rock beats Scissors. Computer wins!')
+    console.log('Rock beats Scissors. Computer wins!');
   } else if (playerSelection === 'Rock' && computer === 'Scissors') {
-    console.log('Rock beats Scissors. Player wins!')
+    console.log('Rock beats Scissors. Player wins!');
   }
 }
 
-function game() {
-    for(let i=1; i <= 5; i++) {
-      playRound("Paper");
-    }
+function getImages(playerSelection,computerSelection) {
+//Player 
+if(playerSelection = 'Rock') {
+  let playerImage = document.querySelector(".playerImg");
+  playerImage.setAttribute('src','./images/rock.png');
+}
+else if(playerSelection = 'Paper') {
+  let playerImage = document.querySelector(".playerImg");
+  playerImage.setAttribute('src','./images/paper.png');
+}
+if(playerSelection = 'Scissors') {
+  let playerImage = document.querySelector(".playerImg");
+  playerImage.setAttribute('src','./images/scissors.png');
 }
 
-game();
+//Computer
+if(computerSelection = 'Rock') {
+  let computerImage = document.querySelector(".computerImg");
+  computerImage.setAttribute('src','./images/rock.png');
+}
+else if(computerSelection = 'Paper') {
+  let computerImage = document.querySelector(".computerImg");
+  computerImage.setAttribute('src','./images/paper.png');
+}
+if(playerSelection = 'Scissors') {
+  let computerImage = document.querySelector(".ComputerImg");
+  computerImage.setAttribute('src','./images/scissors.png');
+}
+}
+
